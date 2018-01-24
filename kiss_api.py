0e79cdf7ee8094dfdb62ef13d32bcc7d08811e5f
Paul Wachendorf <paul.wachendorf@jobmensa.de>
2018-01-24 14:24:47 +0100

    use aiohttp instead of requests for async post to kiss endpoint. stop mainloop on exceptions

--- kiss_api.py
+++ kiss_api.py
@@ -1,8 +1,7 @@
 import json
 import traceback
 import logging
-import requests
-
+import aiohttp
 
 from settings import KISS_BASE_URL
 from settings import KISS_TOKEN_URL
@@ -14,58 +13,69 @@ class KissApi(object):
         self.base_url = KISS_BASE_URL
         self.token_url = '{}{}'.format(KISS_BASE_URL, KISS_TOKEN_URL)
         self.send_url = '{}{}'.format(KISS_BASE_URL, KISS_CLOUDAMQP_ENDPOINT)
-        self.access_token = 'None'
+        self.access_token = None
         self.logger = logging.getLogger('KissApi')
-        self.refresh_access_token()
 
-    def refresh_access_token(self):
+    async def refresh_access_token(self):
         self.logger.debug('getting a new_access token')
-        resp = requests.post(
+        status, data = await self.send_async_post(
             self.token_url,
-            data=KISS_CREDENTIALS,
+            KISS_CREDENTIALS,
             allow_redirects=True,
-            verify=True
+            verify_ssl=True
         )
-        content = resp.content.decode('utf-8')
-        try:
-            data = json.loads(content)
-            self.access_token = data['access_token']
-        except Exception:
-            traceback.print_exc()
-            raise KissApiException(
-                'Unable to get new token. Server response was: "{}"'
-                .format(content)
+        if status != 200:
+            self.logger.error(
+                'Unable to fetch the access token.\n'
+                'Service Response was: {} - {}'.format(status, data)
             )
+            raise KissApiException('unable to fetch api token')
+        self.access_token = data['access_token']
 
-    def send_msg(self, json_data, first=True):
+    async def send_async_post(self, url, data, **kwargs):
+        async with aiohttp.ClientSession() as session:
+            try:
+                resp = await session.post(url, data=data, **kwargs)
+                status = resp.status
+                data = await resp.json()
+                return status, data
+            except aiohttp.client_exceptions.ContentTypeError as e:
+                return status, await resp.text()
+            except aiohttp.client_exceptions.ClientConnectorError as e:
+                return 0, 'Connection to {} failed'.format(url)
+            finally:
+                session.close()
+
+    async def send_msg(self, json_data, first=True):
         self.logger.debug('sending msg')
         if self.access_token is None:
-            self.refresh_access_token()
+            await self.refresh_access_token()
+            self.logger.debug('Recieved new access token')
         headers = {
             'Authorization': 'Bearer {}'.format(self.access_token),
             'content-type': 'application/json',
             'Accept': 'application/json',
         }
-        kwargs = {
-            'allow_redirects': True,
-            'headers': headers,
-            'verify': True,
-            'data': json_data
-        }
-        response = requests.post(self.send_url, **kwargs)
-        content = response.content.decode('utf-8')
-        if response.status_code == 401 and 'Invalid token' in content and first:
+        status, data = await self.send_async_post(
+            self.send_url,
+            json_data,
+            headers=headers,
+            verify_ssl=True,
+            allow_redirects=True
+        )
+        if status == 401 and 'Invalid token' in str(data) and first:
             self.access_token = None
             self.logger.debug('invalid token! retry.')
             return self.send_msg(json_data, first=False)
-        elif response.status_code != 200:
+        elif status != 200:
             self.logger.error(
                 'Error while sending to kiss endpoint.\nMessage was: {}\n'
                 'Response was: {} - {}\n'
-                .format(json_data, response.status_code, content)
+                .format(json_data, status, data)
             )
             raise KissApiException(
-                'error while sending msg to kiss endpoint: {}'.format(content))
+                'error while sending msg to kiss endpoint: {}'.format(data)
+            )
         return response
 
 class KissApiException(Exception):
--- service.py
+++ service.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 import asyncio
 from aio_pika import connect, IncomingMessage
+from datetime import datetime
 import json
+import logging
+import traceback
 
-from kiss_api import KissApi
+from kiss_api import KissApi, KissApiException
 from settings import CLOUD_RABBITMQ_URL
 from settings import CLOUD_RABBITMQ_ROUTING_KEY
 
 kiss_api = KissApi()
+logger = logging.getLogger('Service')
 
 async def on_message(message: IncomingMessage):
     """
@@ -22,34 +26,41 @@ async def on_message(message: IncomingMessage):
         'routing_key': message.routing_key,
         'body': message.body.decode(message.content_encoding or 'utf8')
     })
-    kiss_api.send_msg(msg)
-    print(" [x] Received message %r" % message)
-    await asyncio.sleep(5) # Represents async I/O operations
-    print("After sleep!")
+    response = await kiss_api.send_msg(msg)
+    await asyncio.sleep(5)
 
+def exception_handler(loop, context):
+    e = context.get('exception', None)
+    if not e:
+        logger.error('Unknown Exception: {}'.format(context['message']))
+    elif not isinstance(e, KissApiException):
+        logger.error(
+            ''.join(
+                traceback.format_exception(e, None, e.__traceback__)
+            )
+        )
+        logger.error('{}: {}'.format(e.__class__.__name__, e))
+    loop.stop()
+    loop.call_soon_threadsafe(loop.stop)
 
 async def main(loop):
-    # Perform connection
     connection = await connect(
         CLOUD_RABBITMQ_URL,
         loop=loop
     )
-
-    # Creating a channel
     channel = await connection.channel()
-
-    # Declaring queue
-    queue = await channel.declare_queue('kiss')
-
-    # Start listening the queue with name 'hello'
+    queue = await channel.declare_queue('iao-restrabbit')
     await queue.consume(on_message, no_ack=True)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
+    loop.set_exception_handler(exception_handler)
     loop.create_task(main(loop))
-
-    # we enter a never-ending loop that waits for data and runs callbacks whenever necessary.
-    print(" [*] Waiting for messages. To exit press CTRL+C")
+    logger.debug(
+        " [*] {} Service started. To exit press CTRL+C"
+        .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    )
     loop.run_forever()
+    loop.close()
 
